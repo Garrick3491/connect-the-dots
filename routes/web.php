66f236f3eadb4eae1368daeb490b565a80189d8b
Simon Garrick <simon.garrick@jaywing.com>
2022-11-07 11:13:35 +0000

    doc for text. fix route for capatcha

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
 Route::get('/about', [\App\Http\Controllers\AboutController::class, 'index']);
+Route::get('/reload-captcha', [\App\Http\Controllers\ContactController::class, 'reloadCaptcha']);
